00e63670605f8b247af18a8777242f91bb458ebe
Arpan Roy <arpanroy.dev@gmail.com>
2021-03-13 14:30:16 +0530

    Allow the user to draw by pressing and holding the mouse button.

--- pixel-art/scripts/script.js
+++ pixel-art/scripts/script.js
@@ -1,26 +1,30 @@
 //Main
-const slider = document.querySelector(".slider");
+const slider = document.querySelector("#slider");
 const value = document.querySelector("#value");
 const displayValue = document.createElement('div');
+
+let drawing = false;
+
+//Disable dragging in window
+//This is done to avoid accidently dragging grid cells while drawing.
+window.ondragstart = function() { return false; };
+
+//Display the default grid value
+displayValue.textContent = "Value: " + slider.value;
+value.appendChild(displayValue);
 
 //Render the default grid
 const defaultGridSize = slider.value;
 createGrid(defaultGridSize);
 draw();
 
-//Display the default grid value
-displayValue.textContent = "Value: " + slider.value;
-value.appendChild(displayValue);
-
 //Render the resized grid and updated grid value
 slider.addEventListener('input', function() {
   clearGrid();
   createGrid(this.value);
   displayValue.textContent = "Value: " + this.value;
-  value.replaceChild(displayValue, value.childNodes[0]);
   draw();
 });
-
 
 //clear the grid
 const clear = document.querySelector(".clear");
@@ -33,33 +37,41 @@ function createGrid(gridSize) {
   const grid = document.querySelector(".grid");
   
   for (let i = 0; i < gridSize*gridSize; i++) {
-    const item = document.createElement("div");
-    item.classList.add("item");
-    grid.appendChild(item);
+    const gridCell = document.createElement("div");
+    gridCell.classList.add("grid-cell");
+    grid.appendChild(gridCell);
   }
     
   grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 }
+ 
+function toggleDraw() {
+  document.addEventListener('mousedown', function() {drawing = true});
+  document.addEventListener('mouseup', function() {drawing = false});
+}
 
 function draw() {
-  const items = document.querySelectorAll(".item");
-  items.forEach(item => {
-    item.addEventListener('mouseover', function(e) {
-      e.target.classList.add("ink");
+  const gridCells = document.querySelectorAll(".grid-cell");
+  toggleDraw();
+  gridCells.forEach(gridCell => {
+    gridCell.addEventListener('mouseover', function(e) {
+      if (drawing === true) {
+        e.target.classList.add("ink");
+      }
     });
   });
 }
 
 function clearGridSketch() {
-    const items = document.querySelectorAll(".item")
-    items.forEach(function(item) {
-        item.classList.remove("ink");
+    const gridCells = document.querySelectorAll(".grid-cell")
+    gridCells.forEach(function(gridCell) {
+      gridCell.classList.remove("ink");
     });
 }
 
 function clearGrid() {
-  const items = document.querySelectorAll(".item");
-  items.forEach(function(item) {
-    item.remove();
+  const gridCells = document.querySelectorAll(".grid-cell");
+  gridCells.forEach(function(gridCell) {
+    gridCell.remove();
   });
 }
